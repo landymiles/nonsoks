d99117cef0753f1e4461a3b0b740a719292314f6
landymiles <onwukalamuel@gmail.com>
2022-08-23 12:33:56 +0300

    ,lw,c

---
+++ index.js
@@ -0,0 +1,29 @@
+let controlbtn= document.getElementById("controlbtn");
+controlbtn.addEventListener("click",() => {
+window.location.href = "port test.html";
+});
+
+let controlstn= document.getElementById("controlstn");
+controlstn.addEventListener("click",() => {
+window.location.href = "lol.html";
+});
+
+let controlBtn= document.getElementById("controlBtn");
+controlBtn.addEventListener("click",() => {
+window.location.href = "phones layout.html";
+});
+
+let controlNBtn= document.getElementById("controlNBtn");
+controlNBtn.addEventListener("click",() => {
+window.location.href = "games.html";
+});
+
+let controlCBtn= document.getElementById("controlCBtn");
+controlCBtn.addEventListener("click",() => {
+window.location.href = "vr.html";
+});
+
+let controldBtn= document.getElementById("controldBtn");
+controldBtn.addEventListener("click",() => {
+window.location.href = "cosmetics.html";
+});
