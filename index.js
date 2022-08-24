2a7b902ac23de19c048c9fdda0255db05e1c8c76
landymiles <onwukalamuel@gmail.com>
2022-08-24 15:57:49 +0300

    guyguygyu

--- index.js
+++ index.js
@@ -27,3 +27,21 @@ let controldBtn= document.getElementById("controldBtn");
 controldBtn.addEventListener("click",() => {
 window.location.href = "cosmetics.html";
 });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+} 
+let jlop= document.getElementById("jlop");
+jlop.addEventListener("click",() => {
+window.location.href = "tecno.html";
+});
